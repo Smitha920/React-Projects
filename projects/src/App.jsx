c61a27b67e4c31f8b4dc715e5739c9eea676bb90
Smitha M <smitha92005@gmail.com>
2026-01-30 22:33:25 +0530

    created react

---
+++ projects/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1">Smitha</h1>
+        <h1 id="child2">Hello</h1>
+      </div>
+      <div>
+        <h2>div2</h2>
+      </div>
+    </>
+
+
+  )
+}
+
+export default App
