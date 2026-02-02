a33fcfbdb62d87f760ca458c45b02bbe041c41df
Smitha M <smitha92005@gmail.com>
2026-02-02 22:37:28 +0530

    updated

---
+++ cards/src/App.jsx
@@ -0,0 +1,17 @@
+import {Bookmark} from "lucide-react"
+export const App = () => {
+  return (
+    <div className="parant">
+      <div className="card">
+        <div className="top">
+          <img src="https://toppng.com/public/uploads/preview/amazon-logo-transparent-background-116589592271nenw0vuo4.png" alt=""></img>
+          <button>Save<Bookmark /></button>
+        </div>
+        <div className="center"></div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
