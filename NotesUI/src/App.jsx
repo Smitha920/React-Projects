22d8ec675d8abee02cb595b991c327d4389ad59e
Smitha M <smitha92005@gmail.com>
2026-02-12 21:20:38 +0530

    Updated

--- NotesUI/src/App.jsx
+++ NotesUI/src/App.jsx
@@ -1,36 +1,15 @@
-import { File } from "lucide-react"
-import { Plus } from "lucide-react"
-import { ChevronRight } from "lucide-react"
+import Note from "./components/note.jsx"
+import Msg from "./components/msgs.jsx"
+
 export const App = () => {
   return (
-    <div className="parent">
-      <div className="top">
-        <p><File /> Notes</p>
-        <button><Plus size={20}/>Add Notes</button>
-      </div>
-      <div className="bottom">
-            <button>Task: All</button>
-            <button>Time: All</button>
-            <button>Status: All</button>
-        <div>
-          <div id="msg">
-            <div className="UI">
-              <p>User Interface </p>
-              <span>Tomorrow</span>
-              <span>Incoming</span>
-              <ChevronRight />
-            </div>
-            <p>Showcasing new design elements and styles.</p>
-            <div>
-              <img></img>
-              <img></img>
-              <img></img>
-              <p>Collaborate with <span>Miguel, Jhon, Hane</span></p>
-            </div>
-          </div>
-
-      </div>
-      </div>
+    <div className="parant">
+      <Note />
+      <Msg />
+      <Msg />
+      <Msg />
+      <Msg />
+      <Msg />
     </div>
   )
 }
