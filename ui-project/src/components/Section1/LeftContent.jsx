25f69b654efb1ce8fa870b45b2c582b246fe4e00
Smitha M <smitha92005@gmail.com>
2026-02-16 22:51:25 +0530

    Simple Website UI

---
+++ ui-project/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,20 @@
+import {ArrowUpRight} from "lucide-react"
+
+const LeftContent = () => {
+  return (
+    <div className='h-full flex flex-col w-1/3'>
+        <div className="p-7">
+            <h3 className="mb-7 leading-[1.1] text-7xl font-bold">Propective <br /> <span className="text-gray-600">Customer</span> <br />Segmentation </h3>
+            <p className=" font-medium text-gray-600 text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qvel et libero facere? Ad repellat aspernatur quo autem?</p>
+        </div>
+        <div></div>
+        <div className="text-10xl">
+            <ArrowUpRight />
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default LeftContent
