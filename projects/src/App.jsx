543e8b839b796c123d85504bd6ad545a0b3a8f1a
Smitha M <smitha92005@gmail.com>
2026-02-20 23:20:44 +0530

    Updated

--- projects/src/App.jsx
+++ projects/src/App.jsx
@@ -1,17 +1,13 @@
-const App = () => {
+import Button from "./components/Button/Button"
+import Header from "./components/Header/Header"
+export const App = () => {
   return (
-    <>
-      <div id="parent">
-        <h1 id="child1">Smitha</h1>
-        <h1 id="child2">Hello</h1>
-      </div>
-      <div>
-        <h2>div2</h2>
-      </div>
-    </>
-
+    <div>
+      <Header />
+      <Button />
+    </div>
 
   )
 }
 
-export default App
+export default App;
