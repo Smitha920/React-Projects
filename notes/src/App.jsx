0d3bc7d83faeb9964d76c9f66b9427d8ec8ed90a
Smitha M <smitha92005@gmail.com>
2026-02-23 22:51:32 +0530

    Notes App

---
+++ notes/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="enter Notes heading" />
+        <textarea placeholder="Enter details" name="" id=""></textarea>
+      </form>
+
+    </div>
+  )
+}
+
+export default App
